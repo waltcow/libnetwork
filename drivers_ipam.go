671cb23d17b2fa0667bc466ed7b2b01d71e1d212
waltcow <waltcow@gmail.com>
2016-11-03 21:00:26 +0800

    add experimental Docker Libnetwork DHCP Driver

--- drivers_ipam.go
+++ drivers_ipam.go
@@ -5,12 +5,14 @@ import (
 	"github.com/docker/libnetwork/ipamapi"
 	builtinIpam "github.com/docker/libnetwork/ipams/builtin"
 	nullIpam "github.com/docker/libnetwork/ipams/null"
+	dhcpIpam "github.com/docker/libnetwork/ipams/dhcp"
 	remoteIpam "github.com/docker/libnetwork/ipams/remote"
 )
 
 func initIPAMDrivers(r *drvregistry.DrvRegistry, lDs, gDs interface{}) error {
 	for _, fn := range [](func(ipamapi.Callback, interface{}, interface{}) error){
 		builtinIpam.Init,
+		dhcpIpam.Init,
 		remoteIpam.Init,
 		nullIpam.Init,
 	} {
